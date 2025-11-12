638f623142d7d3c1ba3f89b45789473bea4e9f7f
juniormojapelo <136351518+juniormojapelo@users.noreply.github.com>
2025-11-12 19:38:12 +0200

    users.sql
    updated user.sql

--- users (1).sql
+++ users (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 05, 2025 at 07:31 PM
+-- Generation Time: Nov 12, 2025 at 06:37 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `dressedbygochandvoch`
+-- Database: `dressedbyvochandgoch_db`
 --
 
 -- --------------------------------------------------------
@@ -34,6 +34,15 @@ CREATE TABLE `users` (
   `password_hash` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `full_name`, `email`, `password_hash`, `created_at`) VALUES
+(1, 'Junior Mojapelo', 'juniormojapelo@icloud.com', '$2y$10$F4P1ETYIpaGBV6GXTIH8Lenf51uLxGjpzKVz.ZKz2xuNMwnrpiTbS', '2025-11-12 17:25:12'),
+(2, 'user1', 'user1@gmail.com', '$2y$10$Ekq0Cf0/IB1hbczc0zUlVeA.vvv6fmVoTIl.FA89c6tSUV2HQSfPa', '2025-11-12 17:26:40'),
+(3, 'client 1', 'client1@gmail.com', '$2y$10$mi5z7TcLk0hsl5Nnve2E4.l8zMCpEIxeP2Y11SBFxoAqdqlYn9tQy', '2025-11-12 17:28:21');
 
 --
 -- Indexes for dumped tables
@@ -54,7 +63,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
